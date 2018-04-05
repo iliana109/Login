08f9d4f7882c4a44817767421ea7671dd28f21dc
iliana109 <35733800+iliana109@users.noreply.github.com>
2018-04-05 12:26:57 -0600

    Add files via upload

---
+++ LOGIN 1/script.js
@@ -0,0 +1,11 @@
+var myweb = angular.module("myweb",[]);
+
+myweb.controller("login",function ($scope){
+$scope.usuario="";
+$scope.contra="";
+//function de inicio de sesion
+$scope.Inicio = function(){
+	alert("Ya me puedo graduar")
+
+    }
+});
